28de3918605a7bd91985e1f6651ad7b1415d18a4
sarony <saron.yitbarek@gmail.com>
2016-05-09 02:26:36 -0400

    Makes nav sticky on scroll

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+$("document").ready(function(){
+  var stickyNav = $("nav").offset().top;
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > stickyNav){
+      $("nav").addClass("sticky");
+    }
+    else {
+      $("nav").removeClass("sticky");
+    }
+  });
+});
